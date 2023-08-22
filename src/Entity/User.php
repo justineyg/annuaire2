335269bd6cb98e16c8650221066d4148f3d38f61
Justine Yang <juju-yaj@live.fr>
2023-08-22 17:45:44 +0200

    Ajout "Assert Email'

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -20,6 +20,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     private ?int $id = null;
 
     #[ORM\Column(length: 180, unique: true)]
+    #[Assert\Email()]
     #[Assert\Length(min: 2, max: 50)]
     private ?string $email = null;
 
